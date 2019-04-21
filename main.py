f2ff6996757ba6e02837498c418c941692801b9e
tokyokuma <falblue1052@gmail.com>
2019-04-21 21:48:51 +0900

    ver0.2

--- main.py
+++ main.py
@@ -48,22 +48,22 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='登録したいのは何人？')
-         )
-         if event.message.text.isdigit():
-             num_of_members = event.message.text
-             member_names = []
-             for i in range(0, num_of_people):
-                 line_bot_api.reply_message(
+        )
+        if event.message.text.isdigit():
+            num_of_members = event.message.text
+            member_names = []
+            for i in range(0, num_of_people):
+                line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text='str(num_of_people[i] + 1)'+'目の名前は？')
                  )
                  name[i] = event.message.text
 
          else:
-             line_bot_api.reply_message(
+            line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='数字のみを入力してくだい？')
-             )
+            )
 
     else:
     	line_bot_api.reply_message(
