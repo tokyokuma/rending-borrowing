c70352defbff7c7549d1ecfa08cc74b9b43f8857
tokyokuma <falblue1052@gmail.com>
2019-04-21 22:37:10 +0900

    ver0.2

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def handle_message(event):
         pattern = r'([+-]?[0-9]+\.?[0-9]*)'
         num_of_members = re.match(pattern,event.message.text)
         member_names = []
-        for i in range(0, num_of_people):
+        for i in range(0, num_of_members):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='str(num_of_members[i] + 1)'+'目の名前は？')
