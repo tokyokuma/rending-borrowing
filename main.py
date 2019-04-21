2d178be6c2eee5b23246ddec49c9c53b5ed752c0
tokyokuma <falblue1052@gmail.com>
2019-04-21 21:03:10 +0900

    ver0.2

--- main.py
+++ main.py
@@ -46,10 +46,29 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='「' + event.message.text + '」って何？')
-     )
+    if '登録' in event.message.text:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='登録したいのは何人？')
+         )
+         if event.message.text.isdigit():
+             num_of_members = event.message.text
+             member_names = []
+             for i in range(0, num_of_people):
+                 line_bot_api.reply_message(
+                 event.reply_token,
+                 TextSendMessage(text='str(num_of_people[i] + 1)'+'目の名前は？')
+                 )
+                 name[i] = event.message.text
+
+         else:
+             line_bot_api.reply_message(
+             event.reply_token,
+             TextSendMessage(text='数字のみを入力してくだい？')
+             )
+
+    else:
+        pass
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
