097549784a4e3fa1ea1df7a04c5afece0d8bc5c8
tokyokuma <falblue1052@gmail.com>
2019-04-21 21:37:16 +0900

    ver0.2

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def handle_message(event):
 	line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='「' + event.message.text + '」って何？')
-    )
+     )
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
